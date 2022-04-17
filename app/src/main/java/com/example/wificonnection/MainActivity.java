150075d4abd5d12e71997884985547d8a8ce5445
fhkhd <fhkhdkri@yahoo.com>
2022-04-17 15:48:35 +0430

    set Server clas

--- app/src/main/java/com/example/wificonnection/MainActivity.java
+++ app/src/main/java/com/example/wificonnection/MainActivity.java
@@ -2,11 +2,29 @@ package com.example.wificonnection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +33,22 @@ public class MainActivity extends AppCompatActivity {
     private TextView connection_mode_tv , txt_message , nofind_tv;
     private ListView listView_devices;
     private String string ,st_message;
+
+
+    private WifiP2pManager wifiP2pManager;
+    private WifiP2pManager.Channel channel;
+
+    private BroadcastReceiver broadcastReceiver;
+    private IntentFilter intentFilter;
+
+    private List<WifiP2pDevice> peers = new ArrayList<>();
+    private String[] deviceNameArray;
+
+    private WifiP2pDevice[] deviceArrey;
+
+    private Socket socket;
+
+
 
 
     @Override
@@ -36,4 +70,66 @@ public class MainActivity extends AppCompatActivity {
         listView_devices = findViewById(R.id.listView_devices);
         nofind_tv = findViewById(R.id.nofind_tv);
     }
+
+    public  class Serverclas extends Thread{
+        ServerSocket serverSocket;
+        private InputStream inputStream;
+        private OutputStream outputStream;
+
+        public void write(byte[] bytes)
+        {
+            try {
+                outputStream.write(bytes);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void run(){
+            try {
+                serverSocket = new ServerSocket(8888);
+                socket = serverSocket.accept();
+                inputStream = socket.getInputStream();
+                outputStream = socket.getOutputStream();
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            Handler handler = new Handler(Looper.getMainLooper());
+
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    byte[] buffer = new byte[1024];
+                    int bytes;
+
+                    while (socket != null){
+                        try {
+                            bytes = inputStream.read(buffer);
+                            if (bytes > 0){
+                                int finalBytes = bytes;
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        String tempMSG = new String(buffer , 0 , finalBytes);
+                                        txt_message.setText(tempMSG);
+                                        mssg_ed.setText("");
+                                    }
+                                });
+                            }
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+
+
+
+
 }
